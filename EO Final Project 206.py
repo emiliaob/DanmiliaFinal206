79c6d5d97ba4a1ceb83deb0369d6af32d3e6d920
emiliaob <emiliaob@umich.edu>
2021-12-10 12:02:45 -0500

    Updated and added date table

--- EO Final Project 206.py
+++ EO Final Project 206.py
@@ -14,82 +14,83 @@ def readDataFromFile(filename):
     file_data = f.read()
     f.close()
     return file_data
-    # Copied these from hmwk 7- check if I wrote or they were provided
+ 
 
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
     return cur, conn
-     # Copied these from hmwk 7- check if I wrote or they were provided
-def createDates():
+     
+def createDates(conn, cur):
+    cur.execute("Create TABLE IF NOT EXISTS Dates (ID INTEGER PRIMARY KEY, Date DATE)")
     starting_day= datetime.date(2020, 6,1)
     daysNum=100
     days_list=[]
     for day in range(daysNum):
         date=(starting_day+ datetime.timedelta(days=day)).isoformat()
         days_list.append(date)
+    for i in range(len(days_list)):
+        cur.execute("INSERT OR IGNORE INTO Dates (ID, Date) VALUES (?,?)", (i+1, days_list[i]))
+    conn.commit()
     return(days_list)
+
     
-    # Make the list of 100 dates that I want to use to create request urls for
-    # Can also use range of days
-    # find max date in database- use MAX sql
-def setUpNewsTable(data, cur, conn):
-        cur.execute("CREATE TABLE IF NOT EXISTS Canada(Date DATE,Cases INTEGER, Deaths INTEGER, Tests INTEGER, Vaccinations INTEGER, Total_Cases INTEGER, Total_Deaths INTEGER)")
-        conn.commit()
-        dates= createDates()
     
-        # Need to add the items into the table
 def getData(conn, cur):
     count=0
     date_count=0
     baseurl='https://api.covid19tracker.ca/reports?date={}'
-    cur.execute("CREATE TABLE IF NOT EXISTS Canada(Date DATE PRIMARY KEY,Cases INTEGER, Deaths INTEGER, Tests INTEGER, Vaccinations INTEGER, Total_Cases INTEGER, Total_Deaths INTEGER)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Canada(Date_id INTEGER PRIMARY KEY,Cases INTEGER, Deaths INTEGER, Tests INTEGER, Vaccinations INTEGER, Total_Cases INTEGER, Total_Deaths INTEGER)")
     conn.commit()
-    cur.execute("SELECT COUNT(Date) FROM Canada")
+    cur.execute("SELECT COUNT(Date_id) FROM Canada")
     table_length= cur.fetchone()[0]  
-    dates= createDates()
+    dates= createDates(conn, cur)
     if table_length > 75:
         for i in range(100-table_length):
             if 100- table_length <0:
                 break
             else:
-                cur.execute("SELECT MAX(Date) FROM Canada")
+                cur.execute("SELECT MAX(Date_id) FROM Canada")
                 max_date=cur.fetchone()[0]
-                max_date_index= dates.index(max_date)+1
-                requestsurl= baseurl.format(dates[max_date_index])
+                # max_date_index= dates.index(max_date)
+                requestsurl= baseurl.format(dates[max_date])
                 day= requests.get(requestsurl).content
                 day_data=json.loads(day)
-                insert_tup= (day_data['data'][0]['date'], day_data['data'][0]['change_cases'], day_data['data'][0]['change_fatalities'], day_data['data'][0]['change_tests'], day_data['data'][0]['change_vaccinations'], day_data['data'][0]['total_cases'], day_data['data'][0]['total_fatalities'])
-                cur.execute("INSERT OR IGNORE INTO Canada (Date, Cases, Deaths, Tests, Vaccinations, Total_Cases, Total_Deaths) VALUES (?,?,?,?,?,?,?)", insert_tup)
+                cur.execute("SELECT ID from Dates WHERE Date=?", (day_data['data'][0]['date'],))
+                id_date= cur.fetchone()[0]
+                insert_tup= (id_date, day_data['data'][0]['change_cases'], day_data['data'][0]['change_fatalities'], day_data['data'][0]['change_tests'], day_data['data'][0]['change_vaccinations'], day_data['data'][0]['total_cases'], day_data['data'][0]['total_fatalities'])
+                cur.execute("INSERT OR IGNORE INTO Canada (Date_id, Cases, Deaths, Tests, Vaccinations, Total_Cases, Total_Deaths) VALUES (?,?,?,?,?,?,?)", insert_tup)
                 conn.commit()
     else:
         for i in range(25): 
-            cur.execute("SELECT COUNT(Date) FROM Canada")
+            cur.execute("SELECT COUNT(Date_id) FROM Canada")
             table_length= cur.fetchone()[0]   
             if table_length <=25:
-                cur.execute("SELECT MAX(Date) FROM Canada")
+                cur.execute("SELECT MAX(Date_id) FROM Canada")
                 max_date=cur.fetchone()[0]
                 if max_date==None:
                     requestsurl= baseurl.format(dates[count])
                     count+=1
                 else:
-                    max_date_index= dates.index(max_date)+1
-                    requestsurl= baseurl.format(dates[max_date_index])
+                    requestsurl= baseurl.format(dates[max_date])
                 day= requests.get(requestsurl).content
                 day_data=json.loads(day)
-                insert_tup= (day_data['data'][0]['date'], day_data['data'][0]['change_cases'], day_data['data'][0]['change_fatalities'], day_data['data'][0]['change_tests'], day_data['data'][0]['change_vaccinations'], day_data['data'][0]['total_cases'], day_data['data'][0]['total_fatalities'])
-                cur.execute("INSERT OR IGNORE INTO Canada (Date, Cases, Deaths, Tests, Vaccinations, Total_Cases, Total_Deaths) VALUES (?,?,?,?,?,?,?)", insert_tup)
+                cur.execute("SELECT ID from Dates WHERE Date=?", (day_data['data'][0]['date'],))
+                id_date= cur.fetchone()[0]
+                insert_tup= (id_date, day_data['data'][0]['change_cases'], day_data['data'][0]['change_fatalities'], day_data['data'][0]['change_tests'], day_data['data'][0]['change_vaccinations'], day_data['data'][0]['total_cases'], day_data['data'][0]['total_fatalities'])
+                cur.execute("INSERT OR IGNORE INTO Canada (Date_id, Cases, Deaths, Tests, Vaccinations, Total_Cases, Total_Deaths) VALUES (?,?,?,?,?,?,?)", insert_tup)
                 conn.commit()
             else:
-                cur.execute("SELECT MAX(Date) FROM Canada")
+                cur.execute("SELECT MAX(Date_id) FROM Canada")
                 max_date=cur.fetchone()[0]
-                max_date_index= dates.index(max_date)+1
-                requestsurl= baseurl.format(dates[max_date_index])
+                requestsurl= baseurl.format(dates[max_date])
                 day= requests.get(requestsurl).content
                 day_data=json.loads(day)
-                insert_tup= (day_data['data'][0]['date'], day_data['data'][0]['change_cases'], day_data['data'][0]['change_fatalities'], day_data['data'][0]['change_tests'], day_data['data'][0]['change_vaccinations'], day_data['data'][0]['total_cases'], day_data['data'][0]['total_fatalities'])
-                cur.execute("INSERT OR IGNORE INTO Canada (Date, Cases, Deaths, Tests, Vaccinations, Total_Cases, Total_Deaths) VALUES (?,?,?,?,?,?,?)", insert_tup)
+                cur.execute("SELECT ID from Dates WHERE Date=?", (day_data['data'][0]['date'],))
+                id_date= cur.fetchone()[0]
+                insert_tup= (id_date, day_data['data'][0]['change_cases'], day_data['data'][0]['change_fatalities'], day_data['data'][0]['change_tests'], day_data['data'][0]['change_vaccinations'], day_data['data'][0]['total_cases'], day_data['data'][0]['total_fatalities'])
+                cur.execute("INSERT OR IGNORE INTO Canada (Date_id, Cases, Deaths, Tests, Vaccinations, Total_Cases, Total_Deaths) VALUES (?,?,?,?,?,?,?)", insert_tup)
                 conn.commit()
 
 
@@ -97,6 +98,7 @@ def getData(conn, cur):
 def main():
     cur, conn = setUpDatabase('Covid.db')
     getData(conn, cur)
+    createDates(conn, cur)
         #  How to set up conn and cur?
 
 if __name__ == "__main__":
@@ -105,7 +107,4 @@ if __name__ == "__main__":
 
         
 
-
-
-
-# Shared table for Country/ state area
+# Need to make a date, and a date range table
